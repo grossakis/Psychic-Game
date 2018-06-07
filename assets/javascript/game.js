9994ef74cd4e7d44e6ad9780411ce2e10c331dec
George Rossakis <george@Georges-MacBook-Pro.local>
2018-06-07 10:08:16 -0400

    finalize game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,56 @@
-var guesses = 10;
-
 var letterArray = "abcdefghijklmnopqrstuvwxyz".split("");
-
-var mysteryLetter = letterArray[Math.floor(Math.random()*letterArray.length)];
-console.log(mysteryLetter)
 var wins = 0;
 var losses = 0;
 
-var guessedLetters = document.getElementById("guessed-letters");
-var remainingGuess = document.getElementById("guess-count");
-remainingGuess.textContent = guesses;
-
 var winCount = document.getElementById("win-count");
 winCount.textContent = wins;
+
 var lossCount = document.getElementById("loss-count");
 lossCount.textContent = losses;
 
-document.onkeyup = function() {
-    var letterGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    if(letterGuess === mysteryLetter){
-        alert("youwin");
-        wins++;
-        winCount.textContent = wins;
-    } else if(guesses < 1){
-        alert("youlose");
-        losses++
-        lossCount.textContent = losses;
-    }
-    else{
+function newGame() {
+    var mysteryLetter = letterArray[Math.floor(Math.random()*letterArray.length)];
+    console.log(mysteryLetter)
+    var guesses = 10;
+    var guessedLetters = document.getElementById("guessed-letters");
+    var remainingGuess = document.getElementById("guess-count");
+    remainingGuess.textContent = guesses;
+    var guessArray = [];
+
+    document.onkeyup = function () {
+        var letterGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        if(letterArray.includes(letterGuess)){}
+        else{
+            alert("Please pick a letter")
+            return
+        }
+        for (i = 0; i < guessArray.length; i++){
+            if(letterGuess === guessArray[i]){
+                alert("already guessed");
+                return
+            }
+        }
+        guessArray.push(letterGuess);
+        
         var targetLetters = document.createElement("span");
         targetLetters.textContent = letterGuess + ", ";
         guessedLetters.appendChild(targetLetters);
         guesses--;
         remainingGuess.textContent = guesses;
-    }
-
-    
-    // console.log(letterGuess);
-    
-    // if(guessedLetter === mysteryLetter){
-    //     console.log("correct");
-    // }
-    // else{
-    //     console.log("guess again");
-    // }
-}
+        
+        if(letterGuess === mysteryLetter){
+            alert("You Win!");
+            wins++;
+            winCount.textContent = wins;
+            document.getElementById("guessed-letters").innerHTML = "";
+            newGame();
+        } else if(guesses < 1){
+            alert("You Lose :(");
+            losses++;
+            lossCount.textContent = losses;
+            document.getElementById("guessed-letters").innerHTML = "";
+            newGame();
+        }        
+    };
+};
+newGame();
